a7d4fd69222a933ff10c830c71aa24a078da0d04
Juan Barcelos <juanbarcelos.ti@hotmail.com>
2021-09-13 21:02:42 -0300

    Aula 16 de frontend

--- Backend/Aula02/ex03/js/app.js
+++
@@ -1,9 +0,0 @@
-
-
-function calcular(){
- var   peso = document.getElementsById("peso");
- var  altura = document.getElementsByName("altura");
- var calculo = peso/altura;
-  return  alert(peso.value, altura.value);
-
-}
