a872df9fcd4662fce1208e240fc22f7bf7b4ad2c
Juan Barcelos <juanbarcelos.ti@hotmail.com>
2021-09-15 22:51:13 -0300

    Aula de function

---
+++ Backend/Aula16/app.js
@@ -0,0 +1,21 @@
+function fizzBuzz (n1, n2){
+    for (let i = 1; i <= 100; i++) {
+       if(i%n1 == 0 && i%n2 == 0){
+        console.log("FizzBuzz");
+       } 
+       else if(i%n2 == 0){
+           console.log("Buzz...");
+       }
+       
+       else if(i%n1 == 0 ){
+        console.log("Fizz...");
+            
+       }
+       
+       else{
+        console.log(i);
+       }
+    }
+}
+
+fizzBuzz(3,5);
